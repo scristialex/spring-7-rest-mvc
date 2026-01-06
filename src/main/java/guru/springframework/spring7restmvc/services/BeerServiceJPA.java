e6eabfdcf13e033af74a8bb8a46988602da6c6c7
nafets <scristialex@gmail.com>
2026-01-06 18:49:14 +0200

    JPA services

---
+++ src/main/java/guru/springframework/spring7restmvc/services/BeerServiceJPA.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class BeerServiceJPA implements BeerService {
+    @Override
+    public List<BeerDTO> listBeers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<BeerDTO> getBeerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public BeerDTO saveNewBeer(BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateBeerById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+
+    @Override
+    public void patchBeerById(UUID beerId, BeerDTO beerDTO) {
+
+    }
+}
---
+++ src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.model.CustomerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID uuid) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomerById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customerDTO) {
+
+    }
+}
